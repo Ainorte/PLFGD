1819078a01c3311b4c9cfda9b81ebe9f813b1db9
Flo-Info <florian.regin@etu.unice.fr>
2019-02-08 17:13:04 +0100

    Texte editable android
    Texte editablea sur android + récupération du texte grace à un bouton crée

--- Android/app/src/main/java/unice/plfgd/MainActivity.java
+++ Android/app/src/main/java/unice/plfgd/MainActivity.java
@@ -2,12 +2,39 @@ package unice.plfgd;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+	Button button_edit;
+	// text_bouton c'est pour l'exemple, suffit de la modif, c'est le texte affiche sur le bouton
+	String text_bouton = "Envoi texte";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //On met le text de text_button sur le bouton
+		button_edit = (Button) findViewById(R.id.button_edit);
+		button_edit.setText(text_bouton);
+		addListenerOnButton_edit();
     }
+
+	public void addListenerOnButton_edit() {
+
+		button_edit.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				EditText edit = (EditText)findViewById(R.id.text_input);
+				String res = edit.getText().toString();
+				TextView tv = (TextView)findViewById(R.id.text_edit);
+				tv.setText(res);
+
+			}
+
+		});
+	}
 }
