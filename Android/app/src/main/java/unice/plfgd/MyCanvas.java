ab039f3b23eaaa0af0cf9632b922f4b63a4bf266
Flo-Info <florian.regin@etu.unice.fr>
2019-02-27 16:06:37 +0100

    Changement Main Activity/Canvas
    Canvas pour empeche l'utilisateur d'ecrire

--- Android/app/src/main/java/unice/plfgd/MyCanvas.java
+++ Android/app/src/main/java/unice/plfgd/MyCanvas.java
@@ -22,6 +22,8 @@ public class MyCanvas extends View {
 	Path path;
 	List<Point> coords;
 
+	int b = 1;
+
 	public MyCanvas(Context context, @Nullable AttributeSet attrs) {
 		super(context, attrs);
 
@@ -38,43 +40,45 @@ public class MyCanvas extends View {
 
 	@Override protected void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
-		canvas.drawPath(path,paint);
+		canvas.drawPath(path, paint);
 
 	}
 
 
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
+		if (b == 1) {
+			float xPos = event.getX();
+			float yPos = event.getY();
 
-		float xPos = event.getX();
-		float yPos = event.getY();
+			Point fe = null;
 
-		Point fe = null;
+			switch (event.getAction()) {
+				case MotionEvent.ACTION_DOWN:
+					path.moveTo(xPos, yPos);
+					fe = new Point(xPos, yPos, true);
+					break;
 
-		switch(event.getAction()) {
-			case MotionEvent.ACTION_DOWN:
-				path.moveTo(xPos,yPos);
-				fe = new Point(xPos,yPos, true);
-				break;
+				case MotionEvent.ACTION_MOVE:
+					path.lineTo(xPos, yPos);
+					fe = new Point(xPos, yPos);
+					break;
 
-			case MotionEvent.ACTION_MOVE:
-				path.lineTo(xPos,yPos);
-				fe = new Point(xPos,yPos);
-				break;
+				case MotionEvent.ACTION_UP:
+					break;
 
-			case MotionEvent.ACTION_UP:
-				break;
+				default:
+					return false;
+			}
 
-			default:
-				return false;
+			if (fe != null && !coords.contains(fe)) {
+				coords.add(fe);
+			}
+
+			invalidate();
+			return true;
 		}
-
-		if (fe != null && !coords.contains(fe)) {
-			coords.add(fe);
-		}
-
-		invalidate();
-		return true;
+		return false;
 	}
 
 }
