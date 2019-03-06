1621528f7977acb6276a18a7d3055c414f9264e2
Rohydre <sushi.soumrany@gmail.com>
2019-03-06 13:47:36 +0100

    Update DrawHandler.java

--- Java/Server/src/main/java/unice/plfgd/server/handler/DrawHandler.java
+++ Java/Server/src/main/java/unice/plfgd/server/handler/DrawHandler.java
@@ -5,13 +5,18 @@ import com.corundumstudio.socketio.SocketIOClient;
 import unice.plfgd.common.data.Draw;
 import unice.plfgd.server.Log;
 
+import static unice.plfgd.common.forme.Identifier.sanitize;
+
 public class DrawHandler extends Handler<Draw> {
 
 	@Override
 	public void onData(SocketIOClient client, Draw data, AckRequest ackSender) {
 		if(data != null) {
-			Log.log(Log.State.GREEN, data.getPts().toString());
-			client.sendEvent("draw",data);
+
+			Log.log(Log.State.GREEN, sanitize(data.getPts(),100).toString());
+			System.out.println("Reduced input from "+data.getPts().size()+" to "+ sanitize(data.getPts(),100).size());
+			client.sendEvent("draw",new Draw(sanitize(data.getPts(),100),data.getWidth(), data.getHeight()));
+
 		}
 	}
 }
