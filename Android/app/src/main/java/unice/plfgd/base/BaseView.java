c66a27d7b20d55d8d5024f1af305adc02cc02707
Artemis <ivan@artemix.org>
2019-03-06 15:24:06 +0100

    Fixed baseView generic type, app policy on local storage

--- Android/app/src/main/java/unice/plfgd/base/BaseView.java
+++ Android/app/src/main/java/unice/plfgd/base/BaseView.java
@@ -2,7 +2,7 @@ package unice.plfgd.base;
 
 import unice.plfgd.tool.Connexion;
 
-public interface BaseView<T> {
+public interface BaseView<T extends BasePresenter> {
 	void setPresenter(T presenter);
 	void onSocketReset(Connexion.ResetSocketMessage message);
 }
