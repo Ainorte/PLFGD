c7b31d885b3a786ac227108aab02a55c7fb55b1f
Rohydre <sushi.soumrany@gmail.com>
2019-03-07 16:06:47 +0100

    Adapted sanitize to new draw format

--- Java/Server/src/main/java/unice/plfgd/server/handler/DrawHandler.java
+++ Java/Server/src/main/java/unice/plfgd/server/handler/DrawHandler.java
@@ -3,7 +3,13 @@ package unice.plfgd.server.handler;
 import com.corundumstudio.socketio.AckRequest;
 import com.corundumstudio.socketio.SocketIOClient;
 import unice.plfgd.common.data.Draw;
+import unice.plfgd.common.forme.Point;
 import unice.plfgd.server.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static unice.plfgd.common.forme.TraitementPoints.sanitize;
 
 public class DrawHandler extends Handler<Draw> {
 
@@ -12,9 +18,15 @@ public class DrawHandler extends Handler<Draw> {
 		if (data != null) {
 			//TODO FIND TOLERANCE IN PERCENTAGE OF DRAW SIZE
 			Log.log(Log.State.GREEN, data.getPoints().toString());
-			//Log.log(Log.State.BLUE,"Reduced input from "+data.getPoints().size()+" to "+ sanitize(data.getPoints(),20).size());
-			//client.sendEvent("draw",new Draw(sanitize(data.getPoints(),20),data.getWidth(), data.getHeight()));
-			client.sendEvent("draw", data);
+			List<List<Point>> points = data.getPoints();
+			List<List<Point>> results = new ArrayList<>();
+			for (List<Point> point:points) {
+				results.add(sanitize(point,20));
+
+			}
+			//Log.log(Log.State.BLUE,"Reduced input from "+data.getPoints().size()+" to "+ sanitize(data.getPoints(),20)/.size());
+			client.sendEvent("draw",new Draw(results,data.getWidth(), data.getHeight()));
+			//client.sendEvent("draw", data);
 		}
 	}
 }
