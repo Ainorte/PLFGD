b9ec29e946f32a3a7771f990ef086495d887e797
Flo-Info <florian.regin@etu.unice.fr>
2019-03-20 16:16:23 +0100

    Test Point
    Test de la classe Point

---
+++ Java/Common/src/test/java/unice/plfgd/common/forme/PointTest.java
@@ -0,0 +1,47 @@
+package unice.plfgd.common.forme;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PointTest {
+
+	Point p1;
+	Point p2;
+	Point sym;
+
+	@org.junit.Before
+	public void setUp() throws Exception {
+		p1 = new Point(2,2);
+		p2 = new Point(0,0);
+	}
+
+	@Test
+	public void rotation() {
+		double x = p1.getX();
+		double y = p1.getY();
+		p1.rotation(p2,180);
+		// 180 degre donc x et y ont change de signe
+		// on a une tolerande de 0.00001
+		assertTrue(Math.abs(p1.getX() + x) < 0.00001 &&  Math.abs(p1.getY() + y) < 0.00001);
+	}
+
+	@Test
+	public void getDirection() {
+		// comme precedement on utilise Math.abs et la tolerance
+		assertTrue(Math.abs(p1.getDirection(p2) - Math.atan((p2.getY() - p1.getY()) / (p2.getX() - p1.getX()))) < 0.00001);
+	}
+
+	@Test
+	public void calculeDistanceG() {
+		assertTrue(Math.abs(p1.calculeDistanceG() - Math.sqrt(Math.pow(p1.getX() - p1.getxG(), 2) + Math.pow(p1.getY() - p1.getyG(), 2))) < 0.00001);
+	}
+
+	@Test
+	public void pointSym() {
+		sym = p2.pointSym(p1);
+		double xS = 2 * p2.getX() - p1.getX();
+		double yS = 2 * p2.getY() - p1.getY();
+		assertEquals(sym, new Point(xS, yS));
+	}
+}
