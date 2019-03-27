614f0d13e0564f2a86f19b1cd680b1036557b8bc
Rohydre <sushi.soumrany@gmail.com>
2019-03-27 14:14:15 +0100

    Improved recog

--- Java/Common/src/main/java/unice/plfgd/common/forme/RecogForme.java
+++ Java/Common/src/main/java/unice/plfgd/common/forme/RecogForme.java
@@ -1,5 +1,6 @@
 package unice.plfgd.common.forme;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,8 +8,9 @@ public class RecogForme {
 
 
 	public static List<Object> process(List<Point> pts) {
-		List<Point> convexHull = TraitementPoints.sanitize(pts, 0.02);
-		convexHull = TraitementPoints.refineEndPoints(convexHull, 2);
+		List<Point> sanitized = TraitementPoints.sanitize(pts, 0.02);
+
+		List<Point >convexHull = TraitementPoints.refineEndPoints(sanitized, 2);
 		convexHull = TraitementPoints.closeStroke(convexHull);
 		convexHull = ConvexHull.getConvexHull(convexHull);
 		if (convexHull == null) {
@@ -19,12 +21,16 @@ public class RecogForme {
 		}
 		List<Point> encTriangle = TraitementPoints.maximumAreaEnclosedTriangle(convexHull);
 		List<Point> encRectangle = RotatingCalipers.getMinimumBoundingRectangle(convexHull);
-		return recog(convexHull, encTriangle, encRectangle);
+		return recog(sanitized, convexHull, encTriangle, encRectangle);
 	}
 
-	public static List<Object> recog(List<Point> convexHull, List<Point> encTriangle, List<Point> encRectangle) {
+	public static List<Object> recog(List<Point> pts , List<Point> convexHull, List<Point> encTriangle, List<Point> encRectangle) {
 
 		List<Object> res = new ArrayList<>();
+
+		List<Double> angle = getBigAngle(convexHull);
+		int angleSize = angle.size();
+		System.out.println("ANGLE NUMBER: "  + angleSize);
 
 		Triangle triangle = new Triangle(new Point(0, 0), encTriangle.get(0), encTriangle.get(1), encTriangle.get(2), 0);
 		double triangleArea = triangle.getAire();
@@ -47,16 +53,16 @@ public class RecogForme {
 		double convexTriangleRatio = triangleArea / convexHullArea;
 		double convexRectangleRatio = convexHullPerim / rectanglePerim;
 
-		if (thinnessRatio > 50) {
+		if (thinnessRatio > 50 && angleSize < 1) {
 			res.add(Forme.SEGMENT);
 			res.add(new Segment(convexHull.get(0), convexHull.get(convexHull.size() - 1)));
-		} else if (thinnessRatio < 12.5) {
+		} else if (thinnessRatio < 12.5 && angleSize < 1) {
 			res.add(Forme.CIRCLE);
 			res.add(new Cercle(MethodesForme.barycentre(convexHull), rectanglePerim / (2 * Math.PI), 0));
-		} else if (convexTriangleRatio > 0.7 & convexTriangleRatio < 1) {
+		} else if (convexTriangleRatio > 0.7 & convexTriangleRatio < 1 && angleSize < 4) {
 			res.add(Forme.TRIANGLE);
 			res.add(triangle);
-		} else if (convexRectangleRatio > 0.85 & convexRectangleRatio < 1.25) {
+		} else if (convexRectangleRatio > 0.85 & convexRectangleRatio < 1.25 && angleSize < 5) {
 			if (convexRectangleRatio > 0.9 & convexRectangleRatio < 1.1) {
 				Point G = MethodesForme.barycentre(convexHull);
 				Carre carre = new Carre(G, rectanglePerim / 4, 0);
@@ -71,5 +77,32 @@ public class RecogForme {
 			res.add(new Inconnu(convexHull));
 		}
 		return res;
+	}
+
+
+
+	public static List<Double> getBigAngle(List<Point> pointList){
+
+	List<Double> angleList = new ArrayList<>();
+		for (int i = 1; i < pointList.size()-1; i++) {
+			Point P1 = pointList.get(i-1);
+			Point P2 = pointList.get(i);
+			Point P3 = pointList.get(i+1);
+
+			if(Math.abs(P1.getxV()) > Math.abs(P2.getxV()) && Math.abs(P2.getxV()) < Math.abs(P3.getxV()) &&
+					Math.abs(P1.getyV()) > Math.abs(P2.getyV()) && Math.abs(P2.getyV()) < Math.abs(P3.getyV())	)
+			{
+				double angle = Math.atan2(P3.getY() - P1.getY(), P3.getX() - P1.getX()) - Math.atan2(P2.getY() - P1.getY(), P2.getX() - P1.getX());
+
+
+				if (Math.abs(angle) >= Math.PI / 6) {
+					angleList.add(angle);
+				}
+			}
+		}
+
+		return angleList;
+
+
 	}
 }
